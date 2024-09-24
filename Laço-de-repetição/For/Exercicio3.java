2253a73857ae846a3d1625610d573e014a3b39dd
LuizHenrique-jpg <luiz.mairink@etec.sp.gov.br>
2024-09-24 14:42:12 -0300

    Add files via upload

---
+++ Laço-de-repetição/For/Exercicio3.java
@@ -0,0 +1,17 @@
+package exercicioFor;
+public class Exercicio3 {
+	public static void main(String[] args) {
+		
+	
+		
+		for(int i=1;i<101;i++) {
+
+		if(i%10!=0  ) {
+			System.out.println(i);
+}
+		else {
+			System.out.println(i+" É múltiplo de 10!");
+	 }
+	}
+  }
+}
